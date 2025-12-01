a2ebcc04545c2d296071aaedd484d4c1d0d93081
zhengwenk <zhengwkai@gmail.com>
2025-12-02 00:50:40 +0800

    豆包抓取

--- src/do.cjs
+++ src/do.cjs
@@ -9,7 +9,7 @@ const execOnceLimit = 100;
 
 (async () => {
     const scrapeService = new ScrapeService();
-
+    let browser;
     try {
         // 获取环境变量确定是那个账号id
         const inputAccountId = Number(process.env.Ai_ACCOUNT_ID);
@@ -36,10 +36,24 @@ const execOnceLimit = 100;
             return false;
         }
 
+        const userDataDir = process.env.PUPPETEER_CHROME_USER_DATA_DIR + `/${aiAccount.user_name}`;
+
+        browser = await createBrowser({
+            headless: "new",
+            userDataDir: userDataDir,
+            // args: [
+            //     '--proxy-server=http://110.185.105.144:50004'
+            // ]
+        });
+
+        const page = await createPage(browser);
+        // 监听 浏览器的console
+        page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+
         await asyncForEach(list, async (item, index) => {
             // 开始任务
             console.log(`开始处理任务, 任务ID: ${item.id}`);
-            let browser;
+
             try {
                 const resultId = await scrapeService.startTaskPlan(item)
 
@@ -66,22 +80,9 @@ const execOnceLimit = 100;
                     return;
                 }
 
-                const userDataDir = process.env.PUPPETEER_CHROME_USER_DATA_DIR + `/${aiAccount.user_name}`;
+
 
                 //removeDirSync(userDataDir);
-
-                browser = await createBrowser({
-                    headless: "new",
-                    userDataDir: userDataDir,
-                    // args: [
-                    //     '--proxy-server=http://110.185.105.144:50004'
-                    // ]
-                });
-
-                const page = await createPage(browser);
-                // 监听 浏览器的console
-                page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-
                 // 打开目标页面
                 console.log(aiAccount.url);
 
@@ -99,7 +100,7 @@ const execOnceLimit = 100;
                 console.log(`任务成功, 等待下次任务......`);
 
                 // 增加请求的间隔
-                await new Promise(r => setTimeout(r, randomInt(3000, 10000)));
+                await new Promise(r => setTimeout(r, randomInt(10000, 30000)));
             } catch (err) {
                 console.error("\n--- 程序发生致命错误 ---");
                 console.error("错误详情:", err.message);
@@ -110,11 +111,6 @@ const execOnceLimit = 100;
                     // 如果 error 对象没有 stack 属性 (例如它只是一个简单的字符串拒绝)，
                     // 也可以直接输出完整的 error 对象
                     console.error("完整错误对象:", err);
-                }
-            } finally {
-                if (browser) {
-                    await browser.close();
-                    console.log("浏览器已关闭。");
                 }
             }
         });
@@ -132,5 +128,10 @@ const execOnceLimit = 100;
         }
     } finally {
         await scrapeService.destroyDB()
+
+        if (browser) {
+            await browser.close();
+            console.log("浏览器已关闭。");
+        }
     }
 })();
