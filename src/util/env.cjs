2ddafcb82bb1efe7e8bac39f228c14aa06d68a63
zhengwenk <zhengwkai@gmail.com>
2025-12-03 14:10:11 +0800

    豆包抓取

--- src/util/env.cjs
+++ src/util/env.cjs
@@ -5,7 +5,7 @@ function detectOS() {
     //console.log(platform); // 可能输出 'win32', 'darwin', 'linux', 'aix', 'freebsd', 'sunos'
     let osStr = '';
     if (platform === 'win32') {
-        osStr = 'Win';
+        osStr = 'win';
     } else if (platform === 'darwin') {
         osStr = 'macOS';
     } else if (platform === 'linux') {
