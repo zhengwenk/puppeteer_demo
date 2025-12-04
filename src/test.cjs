298010de01aa6a62caa62fa078b70e3104f38f1f
zhengwenk <zhengwkai@gmail.com>
2025-12-04 17:37:42 +0800

    deepseek抓取

--- src/test.cjs
+++ src/test.cjs
@@ -1,14 +1,29 @@
-const {
-    TaskExecutePlanModel,
-    AiAccountModel,
-    TaskExpandQuestionModel,
-} = require('./models/index.cjs');
+const {lockProcess} = require("./util/lock.cjs");
+const fs = require("fs");
 
-const {fetchProxy} = require("./util/proxy.cjs");
+const aiAccountId = Number(process.env.AI_ACCOUNT_ID) || 0;
+const lockFileDir = process.env.PUPPETEER_LOCK_FILE_DIR || '';
 
-(async () => {
-    const aiAccountId = Number(process.env.AI_ACCOUNT_ID) || 0;
+if (aiAccountId <= 0) {
+    console.error(`请设置正确的AI_ACCOUNT_ID环境变量`);
+    process.exit(1);
+}
 
+// 目录不存在
+if (!fs.existsSync(lockFileDir)) {
+    console.error(`PUPPETEER_LOCK_FILE_DIR`);
+    process.exit(1);
+}
+const lockFile = `${lockFileDir}/do_${aiAccountId}.lock`;
+const ok = lockProcess(lockFile, process.pid);
+if (!ok) {
+    console.error('已有进程在运行，退出。');
+    process.exit(1);
+}
+console.log('进程锁定成功，继续执行。');
+console.log(`Process ID: ${process.pid}`);
 
-
-})();
+// 模拟进程执行
+setInterval(() => {
+    console.log("doing.......")
+}, 3000);
